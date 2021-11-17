80c3a67f0816ba5d4671c9acf0d336940af31803
Yixuan Pan <ypan35@my.bcit.ca>
2021-11-16 22:45:31 -0800

    API call barebone

--- app/src/main/java/ca/bcit/co_green/MainActivity.java
+++ app/src/main/java/ca/bcit/co_green/MainActivity.java
@@ -2,12 +2,20 @@ package ca.bcit.co_green;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToInput(View view) {
+
+        Intent k = new Intent(this, InputActivity.class);
+        startActivity(k);
     }
 }
